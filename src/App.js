decaa0b6e2433f4b672147753c160a630d140ba4
evanegas121 <Elizabeth.Vanegas11@myhunter.cuny.edu>
2021-03-21 17:23:52 -0400

    test

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import SearchBar from './components/functionalComponents/search_bar';
 import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
 import {Home, Search} from './components/views';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import './App.css';
+//import './App.css';
 
 function App() {
   return (
